cb031979eb982a7690ea97b2e0974b979ba68fab
Alvaro <arcas0803@gmail.com>
2024-01-29 16:32:19 +0100

    fix imports

--- lib/appwrite_auth_client.dart
+++ lib/appwrite_auth_client.dart
@@ -1,7 +1,8 @@
 library appwrite_auth_client;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'localization/appwrite_auth_localizations.dart';
+export 'src/appwrite_auth_client.dart';
+export 'src/appwrite_auth_client_impl.dart';
+export 'src/appwrite_user.dart';
+export 'src/auth_failure.dart';
+export 'src/auth_failure_util.dart';
